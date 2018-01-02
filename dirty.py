1793fbff376ed4c9ca3cdaa366cafc6987543476
David Blackman <david@blackmad.com>
2018-01-01 23:31:21 -0500

    going to bed now, 256x256

--- dirty.py
+++ dirty.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # "quickly" convert pngs in parallel in low quality
 # ~/node_modules/.bin/svg2png-many -i output/ -o output/
 # alt
 # find *svg -print0 | xargs -0 -I _ convert _ _.png
 # animate
-#  convert output/*png -loop 0 -coalesce   -duplicate 1,-2-1  anim.gif
+#  convert output/*png -loop 0 -coalesce +duplicate -duplicate 1,-1-0  anim.gif
 # open -a "Google Chrome" anim.gif
 
 import xml.etree.ElementTree as ET
@@ -13,15 +13,45 @@ import urllib.request
 import os
 import os.path
 import shutil
+import argparse
 
 output_dir = 'output'
-show_grids = false
+show_grids = False
 
 if os.path.exists(output_dir):
   shutil.rmtree(output_dir)
 os.makedirs(output_dir)
 
-tree = ET.parse(urllib.request.urlopen('https://raw.githubusercontent.com/googlei18n/noto-emoji/master/svg/emoji_u1f634.svg'))
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--animate", action="store_true")
+parser.add_argument("-t", "--twitter", action="store_true", default=True)
+parser.add_argument("-g", "--google", action="store_true")
+parser.add_argument("codepoint", type=str, help="the emoji codepoint", default="1f634")
+args = parser.parse_args()
+print(args)
+
+codepoint = args.codepoint.replace('U+', '').replace('u+', '').lower()
+
+if args.google:
+  try:
+    url = 'https://raw.githubusercontent.com/googlei18n/noto-emoji/master/svg/emoji_u%s.svg' % codepoint
+    print(url)
+    file = urllib.request.urlopen(url)
+  except:
+    url = 'https://raw.githubusercontent.com/googlei18n/noto-emoji/master/svg/emoji_u%s_200d_2640.svg' % codepoint
+    print(url)
+    file = urllib.request.urlopen(url)
+elif args.twitter:
+  try:
+    url = 'https://raw.githubusercontent.com/twitter/twemoji/gh-pages/2/svg/%s.svg' % codepoint
+    print(url)
+    file = urllib.request.urlopen(url)
+  except:
+    url = 'https://raw.githubusercontent.com/twitter/twemoji/gh-pages/2/svg/%s-1f3fe-200d-2640-fe0f.svg' % codepoint
+    print(url)
+    file = urllib.request.urlopen(url)
+
+tree = ET.parse(file)
 root = tree.getroot()
 
 parent_map = {c:p for p in tree.iter() for c in p}
@@ -32,6 +62,16 @@ def find_all(node, names):
   all = []
   for name in names:
     all += [n for n in node.findall('.//{http://www.w3.org/2000/svg}' + name)]
+
+  def has_defs_in_parent(node):
+    while node in parent_map:
+      node = parent_map[node]
+      if 'defs' in node.tag:
+        return True
+    return False
+
+  all = [a for a in all if not has_defs_in_parent(a)]
+
   return all
 
 def find_all_shapes(node):
@@ -65,14 +105,16 @@ if show_grids:
   toggle_id(root, 'Layer_3')
   write_current_tree(tree)
 
-layer_1 = root.find(".//*[@id='Layer_1']")
+layer_1 = root
+if args.google:
+  layer_1 = root.find(".//*[@id='Layer_1']")
+  if not layer_1:
+    layer_1 = root.find(".//*[@id='图层_1']")
+
+write_current_tree(tree)
 
 for node in find_all_shapes(layer_1):
-  print(count)
-  print(node)
-  print(find_all_shapes(node))
   if len(find_all_shapes(node)) == 0:
-    print(node.attrib)
     node.set('display', 'inherit')
 
     # now set every parent to yes
@@ -88,3 +130,12 @@ if show_grids:
   write_current_tree(tree)
   toggle_id(root, 'Layer_2')
   write_current_tree(tree)
+
+if args.animate:
+  print('rendering to png')
+  # os.system('find output/*svg -print0 | xargs -0 -I _ convert -size 256x256 _ _.png')
+  #os.system('find output/*svg -print0 | xargs -0 -I _ svgexport _ _.png 256:256')
+  os.system('~/node_modules/.bin/svg2png-many -w 256 -h 256 -i output/ -o output/')
+  print('anim')
+  os.system('convert output/*png -loop 0 -coalesce -duplicate 5,-1 -duplicate 1,-1-0 -duplicate 5,0 output/anim.gif')
+  os.system('open -a "Google Chrome" output/anim.gif')
